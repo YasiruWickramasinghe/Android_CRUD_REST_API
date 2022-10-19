3831d605a4bc276a67a259ec51d156870323245b
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2022-10-19 14:04:26 +0530

    Insert Method Completed

--- app/src/main/java/com/example/fuelapp_restapi_android/MainActivity.java
+++ app/src/main/java/com/example/fuelapp_restapi_android/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.fuelapp_restapi_android;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.fuelapp_restapi_android.model.Category;
 
@@ -21,6 +32,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
@@ -69,7 +82,7 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
                         Category cat = new Category();
 //                        cat.setId(jsonObject.getInt("id"));
                         cat.setCategory(jsonObject.getString("title"));
-                        Log.d("TEST", jsonObject.getString("id"));
+//                        Log.d("TEST", jsonObject.getString("id"));
 
 
                         category.add(cat);
@@ -98,8 +111,77 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         recyclerView.setAdapter(categoryAdapter);
     }
 
+//    Insert Function
+
+    public void addCategory(View v){
+        TextView close, judul;
+        EditText cat;
+        Button submit;
+
+        dialog.setContentView(R.layout.activity_modcat);
+
+        close = (TextView) dialog.findViewById(R.id.txtClose);
+        judul = (TextView) dialog.findViewById(R.id.judul);
+
+        judul.setText("set category");
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        cat = (EditText) dialog.findViewById(R.id.cat);
+        submit = (Button) dialog.findViewById(R.id.submit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = cat.getText().toString();
+                Submit(data);
+            }
+        });
+
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
+    }
+
+    private void Submit(String data) {
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                dialog.dismiss();
+                refresh.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        category.clear();
+                        getData();
+                    }
+                });
+
+                Toast.makeText(getApplicationContext(), "Data Inserted Successfully", Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "Data Retrieve Successfully", Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams(){
+                Map<String, String> params = new HashMap<>();
+                params.put("cat", data);
+                return params;
+            }
+        };
+        Volley.newRequestQueue(this).add(request);
+    }
+
+
     @Override
     public void onRefresh() {
-
+        category.clear();
+        getData();
     }
 }
