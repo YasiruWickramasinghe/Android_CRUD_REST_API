8e98fe3684439874b130cd10b201129d78f7de3f
Yasiru Wickramasinghe <it19038292@my.sliit.lk>
2022-10-19 12:28:53 +0530

    get method

--- app/src/main/java/com/example/fuelapp_restapi_android/MainActivity.java
+++ app/src/main/java/com/example/fuelapp_restapi_android/MainActivity.java
@@ -1,13 +1,105 @@
 package com.example.fuelapp_restapi_android;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Dialog;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.fuelapp_restapi_android.model.Category;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private RequestQueue requestQueue;
+    private SwipeRefreshLayout refresh;
+    private ArrayList<Category> category = new ArrayList<>();
+    private JsonArrayRequest arrayRequest;
+    private RecyclerView recyclerView;
+    private Dialog dialog;
+    private CategoryAdapter categoryAdapter;
+
+    private String url ="https://jsonplaceholder.typicode.com/posts";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        refresh = (SwipeRefreshLayout) findViewById(R.id.swipedown);
+        recyclerView = (RecyclerView) findViewById(R.id.category);
+
+        dialog = new Dialog(this);
+
+        refresh.setOnRefreshListener(this);
+        refresh.post(new Runnable() {
+            @Override
+            public void run() {
+                category.clear();
+                getData();
+            }
+        });
+    }
+
+    private void getData() {
+        refresh.setRefreshing(true);
+
+        arrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                JSONObject jsonObject = null;
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        jsonObject = response.getJSONObject(i);
+
+                        Category cat = new Category();
+//                        cat.setId(jsonObject.getInt("id"));
+                        cat.setCategory(jsonObject.getString("title"));
+                        Log.d("TEST", jsonObject.getString("id"));
+
+
+                        category.add(cat);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                adapterPush(category);
+                refresh.setRefreshing(false);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        requestQueue = Volley.newRequestQueue( MainActivity.this);
+        requestQueue.add(arrayRequest);
+    }
+
+    private void adapterPush(ArrayList<Category> category) {
+        categoryAdapter = new CategoryAdapter(this, category);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(categoryAdapter);
+    }
+
+    @Override
+    public void onRefresh() {
+
     }
 }
